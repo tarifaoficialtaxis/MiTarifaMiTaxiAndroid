1e343e8087e446b2f8aa01c78551f04624e86afc
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-06-17 01:53:25 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/components/ui/ButtonLinkRow.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/components/ui/ButtonLinkRow.kt
@@ -1,39 +1,39 @@
 package com.mitarifamitaxi.taximetrousuario.components.ui
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mitarifamitaxi.taximetrousuario.R
 import com.mitarifamitaxi.taximetrousuario.helpers.MontserratFamily
 
 @Composable
-fun CustomContactBoxView(
-    modifier: Modifier,
-    icon: ImageVector,
+fun ButtonLinkRow(
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
 
     Button(
@@ -42,45 +42,47 @@ fun CustomContactBoxView(
         colors = ButtonDefaults.buttonColors(
             containerColor = Color.White
         ),
-        shape = RoundedCornerShape(8.dp),
-        modifier = modifier
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
             .shadow(
-                elevation = 4.dp,
-                shape = RoundedCornerShape(8.dp)
+                elevation = 6.dp,
+                shape = RoundedCornerShape(10.dp)
             )
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.spacedBy(10.dp),
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
-                .padding(vertical = 15.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 15.dp, vertical = 12.dp)
         ) {
-
-            Box(
-                modifier = Modifier
-                    .size(65.dp)
-                    .border(8.dp, colorResource(id = R.color.yellow2), CircleShape)
-                    .background(colorResource(id = R.color.main), shape = CircleShape),
-                contentAlignment = Alignment.Center
-            ) {
-                Icon(
-                    imageVector = icon,
-                    contentDescription = null,
-                    tint = Color.White,
-                    modifier = Modifier.size(27.dp)
-                )
-            }
 
             Text(
                 text = text,
+                modifier = Modifier
+                    .weight(1f),
                 fontFamily = MontserratFamily,
-                fontWeight = FontWeight.Bold,
                 fontSize = 14.sp,
-                color = colorResource(id = R.color.gray1),
-                textAlign = TextAlign.Center,
-                maxLines = 1
+                color = colorResource(id = R.color.black),
+                fontWeight = FontWeight.Bold
             )
 
+            Box(
+                modifier = Modifier
+                    .size(30.dp)
+                    .background(colorResource(id = R.color.main), shape = CircleShape)
+            ) {
+
+                Icon(
+                    imageVector = Icons.Default.ChevronRight,
+                    contentDescription = "content description",
+                    modifier = Modifier
+                        .align(Alignment.Center)
+                        .size(25.dp),
+                    tint = colorResource(id = R.color.black),
+                )
+            }
         }
     }
+
+
 }
