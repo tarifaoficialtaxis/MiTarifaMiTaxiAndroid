526f44ebb70e1dd3c8c2c5ae11531616ad7216ad
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-06-19 14:14:22 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/StingExtension.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/StingExtension.kt
@@ -1,5 +1,8 @@
 package com.mitarifamitaxi.taximetrousuario.helpers
 
+import android.content.Context
+import com.mitarifamitaxi.taximetrousuario.R
+
 fun String.isValidEmail(): Boolean {
     return this.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
 }
@@ -14,4 +17,16 @@ fun String.isValidPassword(): Boolean {
     val hasSymbol = this.any { !it.isLetterOrDigit() }
 
     return this.length >= minLength && hasNumber && hasSymbol
+}
+
+fun getFirebaseAuthErrorMessage(appContext: Context, errorCode: String): String {
+    return when (errorCode) {
+        "ERROR_INVALID_EMAIL" -> appContext.getString(R.string.error_invalid_email)
+        "ERROR_INVALID_CREDENTIAL" -> appContext.getString(R.string.error_wrong_credentials)
+        "ERROR_USER_NOT_FOUND" -> appContext.getString(R.string.error_user_not_found)
+        "ERROR_USER_DISABLED" -> appContext.getString(R.string.error_user_disabled)
+        "ERROR_TOO_MANY_REQUESTS" -> appContext.getString(R.string.error_too_many_requests)
+        "ERROR_OPERATION_NOT_ALLOWED" -> appContext.getString(R.string.error_operation_not_allowed)
+        else -> appContext.getString(R.string.error_authentication_failed)
+    }
 }
