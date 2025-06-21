81b5bb55a4c945c0e9f44862c863b495fde0177d
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-06-20 23:13:59 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/activities/onboarding/LoginActivity.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/activities/onboarding/LoginActivity.kt
@@ -41,6 +41,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.gson.Gson
@@ -407,4 +408,30 @@ class LoginActivity : BaseActivity() {
             )
         }
     }
+
+    private val sampleUiState = LoginUiState(
+        userName = "usuario@ejemplo.com",
+        password = "••••••••",
+        rememberMe = true,
+        userNameIsError = false,
+        passwordIsError = false,
+        userNameErrorMessage = "",
+        passwordErrorMessage = "",
+        mustCompleteProfile = false,
+        showDialogSelectRole = false
+    )
+
+    @Preview
+    @Composable
+    fun LoginScreenPreview() {
+        LoginScreen(
+            uiState = sampleUiState,
+            onRestorePassword = { },
+            onLoginClicked = { },
+            onRegisterClicked = { },
+            onGoogleSignIn = { }
+        )
+    }
+
 }
+
