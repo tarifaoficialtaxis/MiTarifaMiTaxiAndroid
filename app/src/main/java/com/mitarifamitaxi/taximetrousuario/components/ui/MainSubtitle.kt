4a41a171ef879d32f1c4afcb882606b89d73af0f
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-06-21 21:58:23 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/components/ui/MainSubtitle.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/components/ui/MainSubtitle.kt
@@ -1,7 +1,5 @@
 package com.mitarifamitaxi.taximetrousuario.components.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -10,13 +8,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mitarifamitaxi.taximetrousuario.R
 import com.mitarifamitaxi.taximetrousuario.helpers.MontserratFamily
-
-class MainSubtitle {
-}
 
 @Composable
 fun MainSubtitle(
