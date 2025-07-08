cd145e9476db4baedc6d8d259aa4b8a6b11704da
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-07-08 01:33:37 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/K.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/K.kt
@@ -3,11 +3,11 @@ import androidx.compose.ui.unit.dp
 
 object K {
     const val IS_DEV = true
-    const val GOOGLE_API_KEY = "AIzaSyA_JVIFufi7zeH8nfkrglUg4c2QOjQqCx8"
+    /*const val GOOGLE_API_KEY = "AIzaSyA_JVIFufi7zeH8nfkrglUg4c2QOjQqCx8"
     const val MAPS_API_URL = "https://maps.googleapis.com/maps/api/"
 
     const val OPEN_CAGE_API_URL = "https://api.opencagedata.com/"
-    const val OPEN_CAGE_API_KEY = "47f379ddfb624d799af49cfa336d2b08"
+    const val OPEN_CAGE_API_KEY = "47f379ddfb624d799af49cfa336d2b08"*/
 
     const val NOMINATIM_URL = "https://nominatim.openstreetmap.org/"
 
