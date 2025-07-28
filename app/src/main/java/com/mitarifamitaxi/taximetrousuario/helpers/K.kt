00cc4c25ed8c08b6ac0d6531ca4e27a0211cc4e0
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-07-28 00:51:11 -0400

    New build 1.0.9 in the store

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/K.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/helpers/K.kt
@@ -4,7 +4,7 @@ import com.mitarifamitaxi.taximetrousuario.BuildConfig
 
 object K {
     //const val IS_DEV = BuildConfig.IS_DEV
-    const val IS_DEV = true
+    const val IS_DEV = false
     const val NOMINATIM_URL = "https://nominatim.openstreetmap.org/"
 
     val GENERAL_PADDING = 29.dp
