0cc1d28a266992d8010b75250f3632bf00dee510
tarifaoficialtaxis <tarifaoficialtaxis@gmail.com>
2025-08-01 12:53:15 -0400

    General updates

--- app/src/main/java/com/mitarifamitaxi/taximetrousuario/MyApplication.kt
+++ app/src/main/java/com/mitarifamitaxi/taximetrousuario/MyApplication.kt
@@ -5,7 +5,9 @@ import android.app.Application
 import com.bumptech.glide.Glide
 import com.firebase.ui.storage.images.FirebaseImageLoader
 import com.google.firebase.Firebase
+import com.google.firebase.appcheck.FirebaseAppCheck
 import com.google.firebase.appcheck.appCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 import com.google.firebase.initialize
 import com.google.firebase.storage.StorageReference
 import java.io.InputStream
@@ -17,9 +19,17 @@ class MyApplication : Application() {
         super.onCreate()
 
         Firebase.initialize(this)
-        Firebase.appCheck.installAppCheckProviderFactory(
-            PlayIntegrityAppCheckProviderFactory.getInstance(),
-        )
+
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        if (BuildConfig.DEBUG) {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                DebugAppCheckProviderFactory.getInstance()
+            )
+        } else {
+            firebaseAppCheck.installAppCheckProviderFactory(
+                PlayIntegrityAppCheckProviderFactory.getInstance()
+            )
+        }
 
         Glide.get(this).registry.append(
             StorageReference::class.java,
